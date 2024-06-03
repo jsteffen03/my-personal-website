38ab0c0440084f42a086c24e30b0e3273649d3f4
Justin Steffen <jcsteffen03@gmail.com>
2024-06-03 11:03:47 -0600

    More Text Changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const h3 = document.createElement("h3");
-h3.textContent = "this content added by JavaScript";
-document.querySelector("body").appendChild(h3);
+
+//adding body text with java
+const article = document.createElement("article")
+article.textContent = "I am a 21 year old Computer Software Student, I am currently enrolled in Flatiron School and am Excited to be able to learn how to code. This website will be my documentation that I am familair with the content and can put it to use!";
+document.querySelector("body").appendChild(article);
